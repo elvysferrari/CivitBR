79a6038d00d7b71c40d3fafc058dae6ad7ffdf41
elvysferrari <elvys.ferrari@gmail.com>
2019-03-19 22:03:51 -0300

    alterações na tela

--- src/app/pages/insert-post/insert-post.page.ts
+++ src/app/pages/insert-post/insert-post.page.ts
@@ -6,13 +6,15 @@ import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browse
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/models/user';
-import { ToastController, LoadingController, Platform } from '@ionic/angular';
+import { ToastController, LoadingController, Platform, ActionSheetController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { ImagePicker } from '@ionic-native/image-picker/ngx';
 import { Camera, PictureSourceType, CameraOptions, DestinationType } from '@ionic-native/Camera/ngx';
 import { FilePath } from '@ionic-native/file-path/ngx';
 import { File, FileEntry } from '@ionic-native/File/ngx'
 import { WebView } from '@ionic-native/ionic-webview/ngx';
+
+
 @Component({
   selector: 'app-insert-post',
   templateUrl: './insert-post.page.html',
@@ -21,6 +23,9 @@ import { WebView } from '@ionic-native/ionic-webview/ngx';
 export class InsertPostPage implements OnInit {
   postForm: FormGroup;
   user: User;
+
+  maxImages: number = 5;
+  qualityImages: number = 70;
 
   constructor(private afStorage: AngularFireStorage,
     private _sanitizer: DomSanitizer,
@@ -35,7 +40,8 @@ export class InsertPostPage implements OnInit {
     private filePath: FilePath,
     private platform: Platform,
     private camera: Camera,
-    private webview: WebView) {
+    private webview: WebView,
+    public actionSheetController: ActionSheetController) {
 
     this.postForm = this.formBuilder.group({
       titulo: ['', [Validators.required, Validators.minLength(6)]],
@@ -72,14 +78,14 @@ export class InsertPostPage implements OnInit {
     this.file.copyFile(namePath, currentName, this.file.dataDirectory, newFileName).then(success => {
       let filePath = this.file.dataDirectory + newFileName;
       let resPath = this.pathForImage(filePath);
-      this.images.push({ name: newFileName, url: resPath, file: undefined });      
+      this.images.unshift({ name: newFileName, url: resPath, file: filePath });
     }, error => {
       alert('Error while storing file.');
     });
   }
   takePicture(sourceType: PictureSourceType) {
     var options: CameraOptions = {
-      quality: 100,
+      quality: this.qualityImages,
       sourceType: sourceType,
       saveToPhotoAlbum: false,
       correctOrientation: true,
@@ -122,7 +128,7 @@ export class InsertPostPage implements OnInit {
     this.options = {
       // Android only. Max images to be selected, defaults to 15. If this is set to 1, upon
       // selection of a single image, the plugin will return it.
-      maximumImagesCount: (8 - this.images.length),
+      maximumImagesCount: (this.maxImages - this.images.length),
 
       // max width and height to allow the images to be.  Will keep aspect
       // ratio no matter what.  So if both are 800, the returned image
@@ -133,7 +139,7 @@ export class InsertPostPage implements OnInit {
       //height: 200,
 
       // quality of resized image, defaults to 100
-      quality: 80,
+      quality: this.qualityImages,
 
       // output type, defaults to FILE_URIs.
       // available options are 
@@ -159,6 +165,12 @@ export class InsertPostPage implements OnInit {
     this.userService.getLogged().subscribe((user: User) => {
       this.user = user;
     })
+
+  }
+
+  removePicture(img: ImgModel) {
+    let index = this.images.find(x => x.name == img.name);
+    this.images.splice(this.images.indexOf(index), 1);
   }
 
   upload(event, id) {
@@ -167,28 +179,70 @@ export class InsertPostPage implements OnInit {
     this.images[id].file = event.target.files[0]
   }
 
+
+  startUpload(imgEntry) {
+    return new Promise<Blob>(async (resolve, reject) => {
+      this.file.resolveLocalFilesystemUrl(imgEntry.file)
+        .then(entry => {
+          (<FileEntry>entry).file(file => this.readFile(file).then((blob) => {
+            resolve(blob)
+          }))
+        })
+        .catch(err => {
+          reject(undefined)
+        });
+    })
+
+  }
+
+  readFile(file: any) {
+    return new Promise<Blob>(async (resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const imgBlob = new Blob([reader.result], {
+          type: file.type
+        });
+        resolve(imgBlob)
+      };
+      reader.readAsArrayBuffer(file);
+    })
+
+
+  }
+
   async uploadFirebase() {
     return new Promise<boolean>(async (resolve, reject) => {
+      let index = 1;
+      this.images.forEach(async (img: ImgModel) => {
 
-      for (let i = 0; i < this.images.length; i++) {
-        if (this.images[i].file != undefined) {
-          const randomId = Math.random().toString(36).substring(2);
-          this.filePaths.push(randomId);
-          this.ref = this.afStorage.ref(randomId);
-          this.task = this.ref.put(this.images[i].file);
-          await this.task.snapshotChanges().subscribe(a => {
-            if (a.bytesTransferred == a.totalBytes) {
-              this.filesTranferreds.push(a.ref.fullPath)
-            }
-            this.images[i]["progress"] = (a.bytesTransferred / a.totalBytes) * 100;
-            this.uploadProgress = (a.bytesTransferred / a.totalBytes) * 100;
+        if (img.file != undefined) {
 
-            if (this.uploadProgress == 100) {
-              resolve(true)
+          await this.startUpload(img).then(async (imageFile: Blob) => {
+            const randomId = Math.random().toString(36).substring(2);
+            this.filePaths.push(randomId);
+            this.ref = this.afStorage.ref(randomId);
+            this.task = this.ref.put(imageFile);
+            await this.task.snapshotChanges().subscribe(a => {
+              if (a.bytesTransferred == a.totalBytes) {
+                this.filesTranferreds.push(a.ref.fullPath)
+              }
+              img["progress"] = (a.bytesTransferred / a.totalBytes) * 100;
+              this.uploadProgress = (a.bytesTransferred / a.totalBytes) * 100;
+
+            });
+            if(this.images.length == index){
+              setTimeout(()=> {
+                resolve(true)
+              }, 3000)
+            }else{
+              index++;
             }
           });
         }
-      }
+
+      })
+
+
     })
   }
 
@@ -203,8 +257,10 @@ export class InsertPostPage implements OnInit {
     });
     await loading.present();
 
-    await this.uploadFirebase();
-
+    if(this.images.length > 0){
+      await this.uploadFirebase();
+    }
+    
     let formPost = this.postForm.value as Post;
     formPost.totalComentarios = 0;
     formPost.totalCurtidas = 0;
@@ -235,11 +291,49 @@ export class InsertPostPage implements OnInit {
 
   }
 
+  async selectPhotoType() {
+    if (this.images.length < this.maxImages) {
+      const actionSheet = await this.actionSheetController.create({
+        header: 'Escolha as imagens',
+        buttons: [{
+          text: 'Tirar Foto',
+          icon: 'camera',
+          handler: () => {
+            this.openCam()
+          }
+        }, {
+          text: 'Galeria',
+          icon: 'images',
+          handler: () => {
+            this.getImages();
+          }
+        }, {
+          text: 'Fechar',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+
+          }
+        }]
+      });
+      await actionSheet.present();
+    } else {
+      const toast = await this.toastController.create({
+        message: "Número máx. de fotos atingido.",
+        position: 'top',
+        duration: 3000
+      });
+      toast.present();
+    }
+
+  }
+
+
 }
 
-export class ImgModel {  
+export class ImgModel {
   url: string;
   name: string;
-  file: any;
+  file: string;
 }
 
